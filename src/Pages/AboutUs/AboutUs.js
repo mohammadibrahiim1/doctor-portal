f903d7393b47f0f33c169ed1624c45efa6605076
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-09-23 22:38:13 +0600

    create value section

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -65,24 +65,28 @@ const AboutUs = () => {
           </div>
         </div>
       </div>
-      <div>
-        <div className="flex justify-between items-center">
+
+      <div className="w-[1200px] mx-auto  py-[140px] ">
+        <div className="flex justify-between items-center py-[2.75rem] gap-[13.5rem]">
           <h1 className="text-[#1c1c1c] font-sans text-[2.5rem] font-bold leading-10">Our values that drive success</h1>
-          <p className="w-[536px] font-sans text-[#545454]">
+          <p className="w-[536px] font-sans text-[#545454] text-justify">
             Our team of highly trained professionals uses the latest healing technologies to restore you to pain-free
             health quickly and easily. We thoroughly evaluate & treat all of the contributing root factors related to
             your issue. Includes, but is not limit, your work and home stressors.
           </p>
         </div>
-        <div>
+        <div className="grid grid-cols-4 justify-center items-center gap-[46px]">
           {values.map((value) => (
             <>
-              {" "}
-              <div>
-                
-                
-
-                </div>{" "}
+              <div className="border px-[40px] py-[30px]">
+                <img className="w-[124px] h-[124px] mx-auto " src={value.image} alt="" srcset="" />
+                <div className="text-center">
+                  <h1 className="text-[#1c1c1c] text-[1.25rem] font-semibold font-sans py-4">{value.title}</h1>
+                  <p className="text-sm font-semibold text-[#545454] font-sans">
+                    Our team of highly trained professionals uses the latest healing technologies.
+                  </p>
+                </div>
+              </div>
             </>
           ))}
         </div>

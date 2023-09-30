7f46152b8328d0f2037f99cc9b1bb65fbf9161a3
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-09-30 11:51:29 +0600

    create contaci page

--- src/Pages/Home/Contactus/ContactUs.js
+++ src/Pages/Home/Contactus/ContactUs.js
@@ -1,63 +1,59 @@
 import React from "react";
 
+const contactInfo = [
+  // https://i.ibb.co/RPWnmjb/Icon-2.png
+  // https://i.ibb.co/DYCTCb2/Icon-1.png
+  // https://i.ibb.co/d5qNC21/Icon.png
+  {
+    id: 1,
+    title: "Address",
+    location: "209 North Dunbar St.Santa Monica, CA 90403",
+    icon: "https://i.ibb.co/d5qNC21/Icon.png",
+
+  },
+  {
+    id: 2,
+    title: "Call Us",
+    location: "(+22) 123 - 4567 - 900 (+22) 123 - 4567 - 901",
+    icon: 'https://i.ibb.co/RPWnmjb/Icon-2.png'
+  },
+  {
+    id: 3,
+    title: "E-mail Us",
+    location: "support@doctorate.com yourmail@gmail.com",
+    icon: 'https://i.ibb.co/DYCTCb2/Icon-1.png'
+  },
+]
+
 const ContactUs = () => {
   return (
     <div>
       <section>
-        <div className="hero bg-[url('https://i.ibb.co/4JzLf6k/bg-blue-2.png')] bg-cover">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <div
-              className="card flex-shrink-0 w-full max-w-sm mb-4"
-              style={{ height: "468px", width: "350" }}
-            >
-              <div className="card-body ">
-                <div className="text-center">
-                  <p className="text-xs text-primary font-bold">contact us</p>
-                  <h1 className="text-base font-bold text-white ">
-                    Stay connected with us
-                  </h1>
-                </div>
-                <div className="form-control mt-1">
-                  {/* <label className="label">
-            <span className="label-text">Email</span>
-          </label> */}
-                  <input
-                    type="text"
-                    placeholder="email"
-                    className="input input-bordered text-xs"
-                  />
-                </div>
-                <div className="form-control">
-                  {/* <label className="label">
-            <span className="label-text">Password</span>
-          </label> */}
-                  <input
-                    type="text"
-                    placeholder="subject"
-                    className="input input-bordered text-xs"
-                  />
+        <header>
+          <header className='error_bg_img' >
+            <h1 className='text-[#000] font-semibold font-sans  text-5xl text-center py-[158px] w-[552px] mx-auto  ' >We Are Ready To Assist
+              You In 24x7</h1>
+          </header>
+        </header>
 
-                  {/* <label className="label">
-            <Link to="#" className="label-text-alt link link-hover">Forgot password?</Link >
-          </label> */}
-                </div>
+        <div className="w-[1200px] mx-auto " >
+          <header className="text-center">
+            <button className="lato" >contact us</button>
+            <h2 className="text-[#1c1c1c] font-semibold font-sans text-3xl" >Get In Touch With Us!</h2>
+          </header>
 
-                <div className="form-control">
-                  <textarea
-                    name="message"
-                    id=""
-                    cols="30"
-                    rows="10"
-                    className=" text-xs pt-2 input input-bordered"
-                    placeholder="your message"
-                  ></textarea>
+          <div>
+            <div className="grid grid-cols-3 justify-center items-center gap-5 " >
+              {contactInfo.map(contact => <>
+
+                <div>
+                  <div className="border py-[61px] px-[91px] text-center">
+                    <img className="w-[85px] h-[85px] mx-auto" src={contact.icon} alt="" srcset="" />
+                    <h1 className="text-[20px] font-semibold text-[#000] font-sans py-2" >{contact.title}</h1>
+                    <h1 className="text-[13px] font-semibold text-[#7c7c7c] font-sans">{contact.location}</h1>
+                  </div>
                 </div>
-                <div className="form-control ">
-                  <button className="btn btn-primary text-white font-bold w-1/2 mx-auto mt-1 bg-gradient-to-r from-secondary to-primary hover:from-primary hover:to-secondary">
-                    submit
-                  </button>
-                </div>
-              </div>
+              </>)}
             </div>
           </div>
         </div>

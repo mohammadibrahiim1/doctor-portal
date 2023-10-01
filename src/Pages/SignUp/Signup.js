d878c7f3274e8c63f52574c7b6e26c9981949c91
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-10-02 00:17:30 +0600

    create sign up form

--- src/Pages/SignUp/Signup.js
+++ src/Pages/SignUp/Signup.js
@@ -1,93 +1,69 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const handleGoogleSignup = () => {
+    window.open('http://localhost:5000/auth/google', '_self')
+  }
+
+  const handleFacebookSignup = () => {
+    console.log(' facebook signup');
+  }
+  const handleAppleSignup = () => {
+    console.log('apple signup');
+  }
   return (
     <div>
       <div
-        className="hero"
-        style={{ marginTop: "150px", marginBottom: "100px" }}
+        className="hero bg-[#A7D7C5] py-[100px]"
+
       >
         <div className="hero-content flex-col lg:flex-row-reverse">
-        
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <p className="text-center text-xs text-secondary relative top-5">Sign Up! </p>
+
+          <div className="card flex-shrink-0 w-[698px] h-[698px]  shadow-2xl bg-[#F6FBF9] p-10">
+            <h2 className="text-center text-[55px] text-[#000] font-sans font-semibold">Create An Account </h2>
+            <p className="text-center text-[#32403B] text-[19px] font-sans font-semibold w-[400px] mx-auto" >Create an account to enjoy all the services without any ads for free!</p>
             <div
               className="card-body"
-              style={{ width: "385px", height: "556px" }}
             >
-            
+
               <div className="form-control">
-              
-                <label className="">
-                  <span className="ml-1" style={{ fontSize: "14px" }}>
-                    Name
-                  </span>
-                </label>
+
                 <input
-                  style={{ fontSize: "14px" }}
+
                   type="text"
                   name="name"
                   placeholder="your name"
-                  className="input input-sm w-full input-bordered text-xs"
+                  className="input input-sm w-full input-bordered text-lg capitalize font-sans h-[64px]"
                 />
               </div>
               <div className="form-control">
-                <label className="">
-                  <span className="ml-1" style={{ fontSize: "14px" }}>
-                    Email
-                  </span>
-                </label>
                 <input
-                  style={{ fontSize: "14px" }}
+
                   placeholder="email"
                   type="email"
                   name="email"
-                  className="input input-sm w-full input-bordered text-xs"
+                  className="input input-sm w-full input-bordered text-lg capitalize font-sans h-[64px] my-4"
                 />
               </div>
               <div className="form-control">
-                <label className="">
-                  <span className="ml-1" style={{ fontSize: "14px" }}>
-                    Password
-                  </span>
-                </label>
+
                 <input
-                  style={{ fontSize: "14px" }}
+
                   type="password"
                   name="password"
                   placeholder="password"
-                  className="input input-sm w-full input-bordered text-xs"
+                  className="input input-sm w-full input-bordered text-lg capitalize font-sans h-[64px]"
                 />
-                <label className="">
-                  <Link
-                    to="#"
-                    className="label-text-alt link link-hover text-primary"
-                    style={{ fontSize: "14px" }}
-                  >
-                    Forgot password?
-                  </Link>
-                </label>
+
               </div>
-              <div className="form-control mt-1">
+              <div className="">
                 <button
-                  className=" btn-primary rounded text-white text-xs bg-gradient-to-r from-secondary to-primary hover:from-primary hover:to-secondary"
-                  style={{ width: "290px", height: "2.2rem" }}
+                  className=" capitalize font-sans btn-primary rounded-xl text-white text-[30px] bg-[#84C7AE] h-[60px] w-[391px] flex justify-center items-center mx-auto hover:bg-[#84c7ae] my-4"
                 >
-                 Sign Up
+                  create account
                 </button>
               </div>
-              <div>
-                <p className="text-accent" style={{ fontSize: "12px" }}>
-                  new to doctors portal?{" "}
-                  <Link to="/">
-                    {" "}
-                    <span className="text-primary hover:text-accent">
-                      create new account
-                    </span>{" "}
-                  </Link>{" "}
-                </p>
-              </div>
+
               <div className="flex flex-col w-full">
                 <div
                   className="divider"
@@ -101,13 +77,23 @@ const Signup = () => {
                 </div>
               </div>
 
-              <div className="form-control">
-                <button
-                  className="border rounded text-xs hover:bg-accent hover:text-white uppercase"
-                  style={{ width: "290px", height: "2.2rem", fontSize: "14px" }}
+              <div className="flex justify-center items-center gap-5">
+                <button onClick={handleGoogleSignup}
+                  className=" capitalize font-sans btn-primary rounded-xl text-white text-[13px] bg-[#84C7AE] h-[45px] flex justify-center items-center mx-auto hover:bg-[#84c7ae] my-4 px-5 "
                 >
                   continue with google
                 </button>
+                <button onClick={handleFacebookSignup}
+                  className=" capitalize font-sans btn-primary rounded-xl text-white text-[13px] bg-[#84C7AE] h-[45px] flex justify-center items-center mx-auto hover:bg-[#84c7ae] my-4 px-5"
+                >
+                  continue with facebook
+                </button>
+                <button onClick={handleAppleSignup}
+                  className=" capitalize font-sans btn-primary rounded-xl text-white text-[13px] bg-[#84C7AE] h-[45px] flex justify-center items-center mx-auto hover:bg-[#84c7ae] my-4 px-5"
+                >
+                  continue with Apple
+                </button>
+
               </div>
             </div>
           </div>

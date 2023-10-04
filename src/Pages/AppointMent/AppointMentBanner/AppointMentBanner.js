9153e6a40e558341002168c6cc437363ce2bd4bf
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-10-05 00:22:35 +0600

    appointment form

--- src/Pages/AppointMent/AppointMentBanner/AppointMentBanner.js
+++ src/Pages/AppointMent/AppointMentBanner/AppointMentBanner.js
@@ -1,11 +1,123 @@
 import React from "react";
-import { DayPicker } from "react-day-picker";
-import { format } from "date-fns";
+// import { DayPicker } from "react-day-picker";
+// import { format } from "date-fns";
+import { useForm } from "react-hook-form";
 
 const AppointMentBanner = ({ selectedDate, setSelectedDate }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
   return (
     <div>
-      <div
+      <section>
+        <header className=" w-[1744px] h-[432px] bg-indigo-200 mx-auto">
+          <h1 className="text-[#1C1C1C] text-[40px] font-bold font-sans text-center w-[590px] mx-auto">
+            Wide network of healthcare solutions
+          </h1>
+        </header>
+
+        <div className="w-[1000px] mx-auto bg-[F1F1F1]">
+          <form className="p-[100px]" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex gap-3 items-center py-4 ">
+              <input
+                type="text"
+                placeholder="Enter your name here"
+                {...register("First name", { required: true, maxLength: 80 })}
+                className="input input-bordered border-[#DCDCDC] w-[490px] rounded my-2 font-sans
+              "
+              />
+
+              <input
+                type="text"
+                placeholder="Your Email"
+                {...register("Email", {
+                  required: true,
+                  pattern: /^\S+@\S+$/i,
+                })}
+                className="input input-bordered border-[#DCDCDC] w-[490px] rounded my-1 font-sans
+                "
+              />
+            </div>
+            <div className="flex items-center gap-3">
+              <select
+                className="input input-bordered border-[#DCDCDC] w-[490px] rounded my-2 font-sans
+              "
+                {...register("Title", { required: true })}
+              >
+                <option value="selected">Choose a Department</option>
+                <option value="M">Mr</option>
+                <option value="Mr">Mrs</option>
+                <option value="Mis">Miss</option>
+                <option value="D">Dr</option>
+              </select>
+              <select
+                // name="selectedDepartment"
+                // defaultValue="Choose a Department"
+                className="input input-bordered border-[#DCDCDC] w-[490px] rounded my-2 font-sans
+              "
+                {...register("Title", { required: true })}
+              >
+                <option value="selected">Choose Doctor</option>
+                <option value="M">Mr</option>
+                <option value="Mr">Mrs</option>
+                <option value="Mis">Miss</option>
+                <option value="D">Dr</option>
+              </select>
+            </div>
+
+            <div className="flex items-center gap-4 py-4">
+              <input
+                type="tel"
+                placeholder="Mobile number"
+                {...register("Mobile number", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 12,
+                })}
+                className="input input-bordered border-[#DCDCDC] w-[320px]  rounded my-2 font-sans
+                "
+              />
+              <input
+                className="input input-bordered border-[#DCDCDC] w-[320px]  rounded my-2 font-sans
+               "
+                type="date"
+                placeholder="Mobile number"
+                {...register("Mobile number", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 12,
+                })}
+              />
+
+              <select
+                className="input input-bordered border-[#DCDCDC] w-[320px] rounded my-2 font-sans
+              "
+                {...register("Title", { required: true })}
+              >
+                <option value="selected">Select Time</option>
+                <option value="Mr">Mr</option>
+                <option value="Mrs">Mrs</option>
+                <option value="Miss">Miss</option>
+                <option value="Dr">Dr</option>
+              </select>
+            </div>
+
+            <input
+              className="bg-[#0152A8] w-full text-white font-sans font-semibold  py-3 cursor-pointer"
+              type="submit"
+            />
+          </form>
+          <h6 className="text-[#0152A8] font-sans font-semibold text-[15px] py-5">
+            Tahnk you for your booking. we will call you shortly
+          </h6>
+        </div>
+      </section>
+
+      {/* <div
         className="hero mx-auto bg-[url('https://i.ibb.co/KWdnryZ/landing-bg.png')] bg-cover"
         style={{ height: "700px", width: "1363px" }}
       >
@@ -18,7 +130,7 @@ const AppointMentBanner = ({ selectedDate, setSelectedDate }) => {
       </div>
       <div className="text-center text-primary text-gradient-to-r from-secondary to-primary hover:from-primary hover:to-secondary">
         <p>Available Appointments on {format(selectedDate, "PPPP")} </p>
-      </div>
+      </div> */}
     </div>
   );
 };
